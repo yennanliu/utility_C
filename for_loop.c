10479ea6bcb0423ce4622f2d480bc6a313f2edab
yennanliue <f339339@gmail.com>
2019-07-03 23:57:46 +0800

    update for loop with if condition

--- for_loop.c
+++ for_loop.c
@@ -5,8 +5,19 @@ int main()
     int i;
     int j = 10;
 
+    printf("for loop\n");
     for( i = 0; i <= j; i ++ )
     {
        printf("Hello %d\n", i );
+    }
+
+    printf("for loop with if condition\n");
+    for( i = 0; i <= j; i ++ )
+    {  if ( i < 5 )
+    	{
+       printf("Hello %d\n", i );
+    	}
+      else 
+       { printf("too big \n" ); }
     }
 }
