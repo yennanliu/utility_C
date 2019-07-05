43100b1072afb6377d0818800d8be60f577321bd
yennanliu <f339339@gmail.com>
2019-07-05 16:44:33 +0800

    update while func

--- while.c
+++ while.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
+	printf("while demo 1) \n");
 	int i=1;
 	int sum=0;
 	while( i<=100 )
@@ -9,4 +10,16 @@ int main(void)
 		i++;
 	}
 	printf("%d\n",sum);
+
+	printf("while demo 2)");
+	int a=1; 
+	int b=5;
+	while ( a!= b){
+
+	printf("a : %d\n", a );
+	printf("b : %d\n", b);
+	printf("--> a != b \n");
+	a+=1;
+	}
+	printf("a=b");
 }
